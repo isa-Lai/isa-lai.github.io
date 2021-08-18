85ad5fd4d1c06b26794db133851b25d44676e6a6
Isabelle Lai <49911729+isa-Lai@users.noreply.github.com>
2021-08-18 09:21:18 +0800

    Add files via upload

---
+++ src/script/main.js
@@ -0,0 +1,11 @@
+// Can also be included with a regular script tag
+import Typed from "https://cdn.skypack.dev/typed.js";
+
+var options = {
+  strings: ['Software Developer', 'Fast Learner','Computer Engineering Student','Embedded System Developer'],
+  typeSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+};
+
+var typed = new Typed('.intro__typing', options);
